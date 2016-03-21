24dafaa478579275e12cca9b353bc7e06fefae84
Vladimir Suchy <vs.git@vsuchy.com>
2016-03-21 12:56:45 +0100

    Initialization tests.

--- test/rdialogs_test.rb
+++ test/rdialogs_test.rb
@@ -2,12 +2,25 @@ require 'test_helper'
 
 class RDialogsTest < Minitest::Test
 
+  def setup
+    @dialog = RDialogs.new('whiptail')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::RDialogs::VERSION
   end
 
-  def test_that_it_only_accepts_supported_tools
+  def test_that_it_fails_to_initialize_with_unsupported_tool
     exception = assert_raises(RuntimeError) { RDialogs.new('CocoaDialog') }
-    assert_equal 'CocoaDialog: command not supported', exception.message
+    assert_equal exception.message, 'CocoaDialog: command not supported'
+  end
+
+  def test_that_it_initialize_with_supported_tool
+    assert_equal @dialog.instance_variable_get(:@dialog_cmd), 'whiptail'
+  end
+
+  def test_that_command_exists
+    assert @dialog.send(:command_exists?, 'ruby')
+    refute @dialog.send(:command_exists?, 'xxxx')
   end
 end
